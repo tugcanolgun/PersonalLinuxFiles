0517d96224d5d9bbdcb5f35c904ac19132f7e885
Tugcan Olgun <olgunt@itu.edu.tr>
2018-05-12 23:42:36 +0300

    Add spotify controls

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 import sys
 
-APT = """sudo apt-get install -y libqt4-test qt4-dev-tools tk-dev python-dev libsasl2-dev libqtgui4 tmux libncursesw5-dev papirus-icon-theme libldap2-dev vim-gtk3 python-pyrex libqtcore4 libc6-dev arc-theme libpq-dev qt4-designer touchpad-indicator gcc build-essential python-opengl libgdbm-dev filezilla libgle3 pkg-config python3-dev python3.6-dev python-pyside.qtopengl libsqlite3-dev libqt4-script libssl-dev libbz2-dev libtool libxml2-dev git idle-python2.7 libqt4-dbus libreadline-gplv2-dev python-qt4-gl python3-lxml libxslt1-dev zsh libqt4-network libffi-dev autoconf libqt4-xml python-qt4 htop ranger caca-utils highlight atool w3m poppler-utils mediainfo"""
+APT = """sudo apt-get install -y libqt4-test qt4-dev-tools tk-dev python-dev libsasl2-dev libqtgui4 tmux libncursesw5-dev papirus-icon-theme libldap2-dev vim-gtk3 python-pyrex libqtcore4 libc6-dev arc-theme libpq-dev qt4-designer touchpad-indicator gcc build-essential python-opengl libgdbm-dev filezilla libgle3 pkg-config python3-dev python3.6-dev python-pyside.qtopengl libsqlite3-dev libqt4-script libssl-dev libbz2-dev libtool libxml2-dev git idle-python2.7 libqt4-dbus libreadline-gplv2-dev python-qt4-gl python3-lxml libxslt1-dev zsh libqt4-network libffi-dev autoconf libqt4-xml python-qt4 htop ranger caca-utils highlight atool w3m poppler-utils mediainfo xbindkeys"""
 
 def create_dirs():
     home = Path().home()
@@ -53,6 +53,19 @@ def ppas():
     command = "sudo add-apt-repository ppa:papirus/papirus -y"
     os.system(command)
 
+def spotify():
+    os.system("xbindkeys --defaults > ~/.xbindkeysrc")
+    settings = """"dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
+    XF86AudioPlay
+    "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop"
+    XF86AudioStop
+    "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"
+    XF86AudioNext
+    "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"
+    XF86AudioPrev"""
+    os.system(f"echo {settings} >> ~/.xbindkeysrc")
+    os.system("xbindkeysrc")
+
 def main():
     print("Welcome to easy installation")
     print("System will not be updated from the start, the necessary packages will be installed")
@@ -64,6 +77,7 @@ def main():
     create_dirs()
     oh_my_zsh()
     vundle()
+    spotify()
 
 
 if __name__ == "__main__":
